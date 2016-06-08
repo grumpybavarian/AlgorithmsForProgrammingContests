b89e51873741be39f6a0b87479d8adf214113ebe
Thomas Edlich <thomas.edlich@tum.de>
2016-06-08 16:17:34 +0200

    added project sceleton for week 9

---
+++ Week9/1_One/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    for (int ti=1; ti<=t; ++ti) {
+        
+        cout << "Case #" << ti << ": " << -1 << "\n";
+    }
+    return 0;
+}
