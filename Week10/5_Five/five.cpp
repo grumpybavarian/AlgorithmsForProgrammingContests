40b324ca9f88685fede04463a940feab35cd0960
Thomas Edlich <thomas.edlich@tum.de>
2016-06-15 12:33:53 +0200

    initial commit Week10

---
+++ Week10/5_Five/five.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int ti=1; ti<=t; ++ti) {
+        
+        cout << "Case #" << ti << ": " << -1 << endl;
+    }
+    
+    return 0;
+}
