f11dbbc8ab2742010777534cc4883f4e6d930b4e
Thomas Edlich <thomas.edlich@tum.de>
2016-06-15 18:59:16 +0200

    solution week10 problem A

--- Week10/1_One/one.cpp
+++ Week10/1_One/one.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <math.h>
+#include <cmath>
 using namespace std;
 
 int main() {
     int t;
     cin >> t;
     for (int ti=1; ti<=t; ++ti) {
-        
-        cout << "Case #" << ti << ": " << -1 << endl;
+        int n;
+        cin >> n;
+        int g;
+        cin >> g;
+        for (int i=0; i<n-1; ++i) {
+            int b;
+            cin >> b;
+            g = __gcd(b,g);
+        }
+        cout << "Case #" << ti << ": " << g << endl;
     }
     
     return 0;
 }
+
+/*
+ 2
+ 3
+ 12 6 21
+ 4
+ 24 7 12 18
+*/
