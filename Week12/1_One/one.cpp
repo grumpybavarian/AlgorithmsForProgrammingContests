6eee1892ea3187954db7cf903c253616d963ffb0
Thomas Edlich <thomas.edlich@tum.de>
2016-06-29 21:54:46 +0200

    created project sceleton for week12

---
+++ Week12/1_One/one.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1_One
+//
+//  Created by Thomas Edlich on 29/06/16.
+//  Copyright © 2016 Thomas Edlich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Week12/5_Five/five.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  5_Five
+//
+//  Created by Thomas Edlich on 29/06/16.
+//  Copyright © 2016 Thomas Edlich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
